6e5346d69887da33457e8e8ab9ee97b041c6a5a8
jowayne <jowayne@jowayne-air.mynetworksettings.com>
2024-02-20 12:53:29 -0500

    updated

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Header from './containers/Header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ProductDetails from './containers/ProductDetails';
+import ProductListing from './containers/ProductListing';
+
+function App() {
+  return (
+    <div className="App">
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path = '/' exact element = {<ProductListing/>} />
+        <Route path= '/product/:productId' exact element = {<ProductDetails/>} />
+        <Route>404 Not Found!</Route>
+      </Routes>
+    </Router>
+       
+    </div>
+  );
+}
+
+export default App;
